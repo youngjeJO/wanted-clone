78bd1a0b9f72f0a817e729967c320ce0b91022db
youngjeJO <yj01591@gmail.com>
2022-01-24 15:29:47 +0900

    slide 컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Slide from './components/slide/slide';
+
+function App() {
+  return (
+    <div>
+      <Slide />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/slide/slide.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Slide = (props) => <div>슬라이드</div>;
+
+export default Slide;
