51c2b4046ed8dd0e3c29474b1db80bca481fe1d0
youngjeJO <yj01591@gmail.com>
2022-01-24 16:03:03 +0900

    이미지 가져오기

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -1,5 +1,60 @@
 import React from 'react';
 
-const Slide = (props) => <div>슬라이드</div>;
+const slideImg = [
+  {
+    id: 0,
+    image:
+      'https://static.wanted.co.kr/images/banners/1460/619f3af7.thumb_1006_280.jpg',
+    title: '개발자 성장 비결 공개!',
+    subTitle: 'Velog, 글 쓰는 개발자들의 이야기',
+  },
+  {
+    id: 1,
+    image:
+      'https://static.wanted.co.kr/images/banners/1486/fba2df30.thumb_1006_280.jpg',
+    title: '성과를 내는 마케팅',
+    subTitle: '실제 사례를 공개 합니다!',
+  },
+  {
+    id: 2,
+    image:
+      'https://static.wanted.co.kr/images/banners/1488/baa54448.thumb_1006_280.jpg',
+    title: 'UX 디자이너의 커리어 설계',
+    subTitle: '브랜드 가치를 더하는 디자인',
+  },
+  {
+    id: 3,
+    image:
+      'https://static.wanted.co.kr/images/banners/1468/3df61cbc.thumb_1006_280.jpg',
+    title: '해, 커리어 EP 02 공개',
+    subTitle: '마지막 관문 2라운드의 승자는?',
+  },
+  {
+    id: 4,
+    image:
+      'https://static.wanted.co.kr/images/banners/1473/41f7b36e.thumb_1006_280.jpg',
+    title: '개발자가 되고싶은 분들!?',
+    subTitle: '프론트앤드 무료 교육과정 참여하기',
+  },
+  {
+    id: 5,
+    image:
+      'https://static.wanted.co.kr/images/banners/1484/b2853456.thumb_1006_280.jpg',
+    title: '성장하는 개발자가 되려면?',
+    subTitle: '000 검색하지 말 것!',
+  },
+];
+
+const Slide = (props) => {
+  const slidelist = slideImg.map((item) => (
+    <img key={item.id} src={item.image} alt='' />
+  ));
+  const arr = ['사과', '귤', '수박'];
+  arr.forEach((item) => {
+    console.log(item);
+  });
+
+  return <div>{slidelist}</div>;
+};
 
 export default Slide;
